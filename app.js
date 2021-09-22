758772646a4c046c9fec866e77b095d95b7452ec
jonsnowljs <jonsnowljs@gmail.com>
2021-09-22 16:54:52 -0600

    finished

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const _ = require("lodash");
 
 const app = express();
 
@@ -61,7 +62,7 @@ app.get("/", function(req, res) {
 });
 
 app.get("/:customListName", function(req, res){
-  const customListName = req.params.customListName;
+  const customListName = _.capitalize(req.params.customListName);
 
   List.findOne({name: customListName}, function(err, foundList){
 
@@ -105,25 +106,23 @@ app.post("/", function(req, res){
 app.post("/delete", function(req, res){
   const checkedItemId = req.body.checkbox;
   const listName = req.body.listName;
-
+  
   if (listName === "Today") {
     Item.findByIdAndRemove(checkedItemId, function(err){
       if (!err) {
-        console.log("Successfully deleted checked item.");
+        console.log("Success delete");
         res.redirect("/");
       }
-    });
+    })
   } else {
     List.findOneAndUpdate({name: listName}, {$pull: {items: {_id: checkedItemId}}}, function(err, foundList){
       if (!err){
         res.redirect("/" + listName);
       }
-    });
+    })
   }
 
-
 });  
-
 
 app.get("/about", function(req, res){
   res.render("about");
